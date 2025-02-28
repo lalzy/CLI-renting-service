2e2bd9f500e374bf1e9a2a2ac4c98aaf2dcb53fc
skn <svenyk@gmail.com>
2025-02-28 10:20:14 +0100

    Commenting and Reordering.
    Reordered the code a bit, and added comments.

--- CLI-RentingService.Tests/UnitTest1.cs
+++ CLI-RentingService.Tests/UnitTest1.cs
@@ -4,22 +4,41 @@ using NuGet.Frameworks;
 using System.ComponentModel;
 public class UnitTest1
   {
+    /// <summary>
+    /// Get the count of books from the library.
+    /// </summary>
+    /// <param name="rs">RentingService instance</param>
+    /// <param name="bookTitle">Title of the book</param>
+    /// <returns></returns>
+    private int GetBookCount(RentingService rs, String bookTitle){
+      return (from book in rs.ListAllBooks()
+          where book.Key.Title.ToLower() == bookTitle.ToLower()
+          select book.Value).FirstOrDefault();
+    }
+
+    /// <summary>
+    /// Testing adding a new book to the library.
+    /// </summary>
     [Fact]
     public void AddingNewBook(){
-      string title = "-new-book-added";
+      string bookTitle = "-new-book-added";
       string author = "-somedude-";
       int amount = 2;
 
       RentingService rs = new RentingService();
-      rs.addBook(title, author, amount);
+      rs.addBook(bookTitle, author, amount);
 
-      KeyValuePair<Book, int>? newBook = rs.CheckAvailability(title);
+      KeyValuePair<Book, int>? newBook = rs.CheckAvailability(bookTitle);
       Assert.NotNull(newBook);
-      Assert.Equal(newBook.Value.Key.Title, title);
+      Assert.Equal(newBook.Value.Key.Title, bookTitle);
       Assert.Equal(newBook.Value.Key.Author, author);
       Assert.Equal(newBook.Value.Value, amount);
     }
 
+    /// <summary>
+    /// Testing to check when a book we're asking for is avilable and valid.
+    /// </summary>
+    /// <param name="bookTitle">Title of the book</param>
     [InlineData("Harry Potter")]
     [InlineData("FounDation")]
     [InlineData("harry potter")]
@@ -32,6 +51,10 @@ public class UnitTest1
       Assert.True(ret.Value.Value > 0);
     }
 
+    /// <summary>
+    /// Test for CheckAvailability with a book that doesn't actually exist.
+    /// </summary>
+    /// <param name="bookTitle">Title of the book</param>
     [InlineData("abc")]
     [InlineData("")]
     [InlineData(null)]
@@ -43,61 +66,85 @@ public class UnitTest1
       Assert.Null(ret);
     }
 
-    private int GetBookCount(RentingService rs, String bookTitle){
-      return (from book in rs.ListAllBooks()
-          where book.Key.Title.ToLower() == bookTitle.ToLower()
-          select book.Value).FirstOrDefault();
-    }
 
-    //[InlineData ("Foundation")]
+    /// <summary>
+    /// Test for if we attempt to rent an unavailable book (that is an valid existing book in the library).
+    /// </summary>
+    [Fact]
+    public void IfUnavailableBook(){
+      string bookTitle = "foundation";
+      RentingService rs = new RentingService();
+      int bookCount = GetBookCount(rs, bookTitle);
+
+      for(int i = 0; i < bookCount ; i++){
+        rs.rentBook(bookTitle);
+      }
+
+      var ret = rs.rentBook(bookTitle);
+      Assert.Null(ret);
+    } 
+
+    /// <summary>
+    /// Ensuring renting of a book is handled correctly.
+    /// </summary>
     [Fact]
     public void BookRented(){
-      string title = "foundation";
+      string bookTitle = "foundation";
       RentingService rs = new RentingService();
-      int preBorrowCount = GetBookCount(rs, title);
-      var ret = rs.rentBook(title);
+      int preBorrowCount = GetBookCount(rs, bookTitle);
+      var ret = rs.rentBook(bookTitle);
       Assert.NotNull(ret);
 
-      int postBorrowCount = GetBookCount(rs, title);
+      int postBorrowCount = GetBookCount(rs, bookTitle);
       Assert.NotEqual(preBorrowCount, postBorrowCount);
       Assert.Equal(preBorrowCount - 1, postBorrowCount);
     } 
 
+    /// <summary>
+    /// Test if we are renting an non-existant book.
+    /// </summary>
     [Fact]
     public void RentNonExistentBook(){
       RentingService rs = new RentingService();
       var ret = rs.rentBook(null);
       Assert.Null(ret);
 
-      string title = "--doesn'tactuallyexisthere--";
-      ret = rs.rentBook(title);
+      string bookTitle = "--doesn'tactuallyexisthere--";
+      ret = rs.rentBook(bookTitle);
       Assert.Null(ret);
     }
 
+    /// <summary>
+    /// Testing return of a book
+    /// </summary>
+    /// <param name="bookTitle">title of the book</param>
     [InlineData("harry PottEr")]
     [InlineData("foundation")]
     [Theory]
-    public void returningBook(String title){
+    public void returningBook(String bookTitle){
       RentingService rs = new RentingService();
-      int preBorrow = GetBookCount(rs, title);
+      int preBorrow = GetBookCount(rs, bookTitle);
 
-      BorrowReceipt? br = rs.rentBook(title);
+      BorrowReceipt? br = rs.rentBook(bookTitle);
       Assert.NotNull(br);
 
-      var ret = rs.ReturnBook(title, br.BorrowDate);
+      var ret = rs.ReturnBook(bookTitle, br.BorrowDate);
       Assert.NotNull(ret);
       Assert.IsType<ReturnReceipt>(ret);
-      int postGivenBack = GetBookCount(rs, title);
+      int postGivenBack = GetBookCount(rs, bookTitle);
       Assert.Equal(preBorrow, postGivenBack);
     }
 
+    /// <summary>
+    /// Test if we attempt to return a book that doesn't exist.
+    /// </summary>
     [Fact]
     public void ReturningInvalidBook(){
-      string title = "doesn't exist";
-      BorrowReceipt br = new BorrowReceipt(title);
+      string bookTitle = "doesn't exist";
+      BorrowReceipt br = new BorrowReceipt(bookTitle);
       RentingService rs = new RentingService();
 
-      var ret = rs.ReturnBook(title, br.BorrowDate);
+      var ret = rs.ReturnBook(bookTitle, br.BorrowDate);
       Assert.Null(ret);
     }
   }
